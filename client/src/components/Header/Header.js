4d79ab623643abfa9dfb711acccab9233cbc4ff6
hanbanana <2han@live.com>
2018-09-20 20:49:18 -0700

    welcome page with 1 page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -32,14 +32,14 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div className="bg-primary" >
+      <div className="bg-white" >
         <div className="position-absolute w-100 text-center" style={{ zIndex: '9999', pointerEvents: 'none', marginTop: '10px' }}>
           <h3>Handy Helper</h3>
         </div>
         <div className="container">
           <div className="row">
             <div className="col px-0">
-              <Navbar color="primary" light expand="md">
+              <Navbar color="white" light expand="md">
                 <Link to={localStorage.getItem('id_token') ? '/home' : '/'} className="navbar-brand">HH</Link>
                 <Nav className="ml-auto" navbar>
                   {localStorage.getItem('id_token') ? <Logout onClick={this.handleLogout.bind(this)} /> : <Login />}
