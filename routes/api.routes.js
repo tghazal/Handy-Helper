0e51af78cd2390b72b02124988df16f1a4e8592b
unknown <t.bakeer4@gmail.com>
2018-09-20 19:42:37 -0700

    welcome page

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 var models = require("../models/users.model");
-router.post("", function (req, res) {
 
 
-})
+router.post("/register", function (req, res) {
+    var user = {
+        email: req.body.email,
+        name: req.body.name,
+        
+    }
+   
+
+    models.create(user) 
+    .then(dbModel =>  res.json({ message: "Creation Sucess!", id: dbModel.id }))
+    .catch(err => res.status(422).json(err));
+
+
+  
+});
 
 module.exports = router;
