e4829d9025689224f3b7bf741ba4335364ae81ff
Blake Mason <blake@Blakes-MBP.fios-router.home>
2018-09-26 18:57:57 -0700

    fixed broken stuff

--- client/src/components/pages/Welcome/Welcome.js
+++ client/src/components/pages/Welcome/Welcome.js
@@ -3,7 +3,7 @@ import "./Welcome.css";
 
 const Welcome = (props) => {
   return (
-    <div className="container-fluid d-flex flex-grow-1">
+    <div className="container-fluid flex-grow-1 d-flex flex-column">
       <div className="row flex-grow-1">
         <div className="col p-0 col-style d-flex flex-column align-items-center justify-content-around text-center">
           <h1 className="title display-1 font-weight-bold">
